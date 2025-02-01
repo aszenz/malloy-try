fd7843149f18e929daef917a3f5e3b5484ea0c2b
asrar <asrar@qbil.nl>
2025-02-01 23:56:41 +0100

    chore: Fix format

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.GITHUB_ACTIONS ? '/malloy-try/' : '/',
+  base: process.env.GITHUB_ACTIONS ? "/malloy-try/" : "/",
   plugins: [react()],
   define: {
     "process.env": {},
