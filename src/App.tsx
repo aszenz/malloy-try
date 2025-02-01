8968155f6d3f76501168c8fd49f48720353eee68
asrar <asrar@qbil.nl>
2025-02-01 21:55:57 +0100

    chore: Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-nocheck
-import { useCallback, useEffect, useMemo, useState } from "react";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import * as malloy from "@malloydata/malloy";
 import {
+  EventModifiers,
   useQueryBuilder,
+  StubCompile,
   UndoContext,
   useRunQuery,
   ExploreQueryEditor,
@@ -15,55 +15,67 @@ import { SingleConnectionRuntime } from "@malloydata/malloy";
 import { DuckDBWASMConnection } from "@malloydata/db-duckdb/wasm";
 import "@malloydata/render/webcomponent";
 
+const stubCompile = new StubCompile();
+
 /**
  * https://github.com/malloydata/malloy-composer/blob/main/src/app/Explore/Explore.tsx
  */
 export function App() {
-  const [setup, setRuntime] = useState<{
-    runtime: malloy.Runtime;
-    modelDef: malloy.ModelDef;
-  } | null>(null);
-  useEffect(() => {
-    async function set() {
-      console.log("Setting up runtime");
-      const { runtime, model } = await setupRuntime();
-      setRuntime({ runtime, modelDef: model._modelDef });
-    }
-    set();
-  }, []);
+  const setup = useRuntimeSetup();
 
-  if (!setup) {
+  if (null === setup) {
     return <div>Loading...</div>;
   }
-  return <Test modelDef={setup.modelDef} runtime={setup.runtime} />;
+  return (
+    <ModelExplorer
+      runtime={setup.runtime}
+      modelDef={setup.modelDef}
+      refreshModel={setup.refreshModel}
+    />
+  );
 }
 
-function Test({
-  modelDef,
+function ModelExplorer({
   runtime,
+  modelDef,
+  refreshModel,
 }: {
-  modelDef: malloy.ModelDef;
   runtime: malloy.Runtime;
+  modelDef: malloy.ModelDef;
+  refreshModel: () => void;
 }) {
   const modelPath = "./";
   const sourceName = "test";
+  const history = useRef<string[]>([""]);
+  const historyIndex = useRef(0);
+
+  const updateQueryInUrl = useCallback(
+    ({ query }: { query: string | undefined }) => {
+      if (query && query !== history.current[historyIndex.current]) {
+        history.current = history.current.slice(0, historyIndex.current + 1);
+        history.current.push(query);
+        historyIndex.current++;
+      }
+      console.info("updateQueryInUrl", history.current, historyIndex.current);
+    },
+    []
+  );
   const {
     error: builderError,
     queryWriter,
     queryModifiers,
     querySummary,
-  } = useQueryBuilder(modelDef, sourceName, modelPath, () => {});
+  } = useQueryBuilder(modelDef, sourceName, modelPath, updateQueryInUrl);
   const {
     error: runnerError,
     result,
     runQuery,
-    // reset,
     isRunning,
   } = useRunQuery(
     modelDef,
     modelPath,
     (query: string, model: malloy.ModelDef) =>
-      runQueryExt(runtime, query, model)
+      executeMalloyQuery(runtime, query, model)
   );
 
   const source = modelDef.contents["test"] as SourceDef;
@@ -78,52 +90,55 @@ function Test({
     }
   }, [queryName, queryWriter, runQuery]);
 
-  // const {topValues, refresh: refreshTopValues} = useTopValues(
-  //   modelDef,
-  //   modelPath,
-  //   sourceDef
-  // );
+  const { topValues, refresh: refreshTopValues } = useTopValues(
+    runtime,
+    modelDef,
+    source,
+    modelPath
+  );
 
-  // const refresh = useCallback(
-  //   ({shiftKey}: EventModifiers) => {
-  //     refreshModel();
-  //     if (shiftKey) {
-  //       refreshTopValues();
-  //     }
-  //   },
-  //   [refreshModel, refreshTopValues]
-  // );
+  console.log({ topValues });
+
+  const refresh = useCallback(
+    ({ shiftKey }: EventModifiers) => {
+      refreshModel();
+      if (shiftKey) {
+        refreshTopValues();
+      }
+    },
+    [refreshModel, refreshTopValues]
+  );
 
   const undoContext = useMemo(() => {
-    // const updateQuery = () => {
-    //   const query = history.current[historyIndex.current];
-    //   if (query) {
-    //     stubCompile
-    //       .compileQuery(modelDef, query)
-    //       .then(query => queryModifiers.setQuery(query, true))
-    //       .catch(console.error);
-    //   } else {
-    //     queryModifiers.clearQuery(true);
-    //   }
-    // };
+    const updateQuery = () => {
+      const query = history.current[historyIndex.current];
+      if (query) {
+        stubCompile
+          .compileQuery(modelDef, query)
+          .then((query: string) => queryModifiers.setQuery(query, true))
+          .catch(console.error);
+      } else {
+        queryModifiers.clearQuery(true);
+      }
+    };
 
-    const canRedo = () => false; // historyIndex.current < history.current.length - 1;
-    const canUndo = () => false; //historyIndex.current > 0;
+    const canRedo = () => historyIndex.current < history.current.length - 1;
+    const canUndo = () => historyIndex.current > 0;
 
     const undo = () => {
       if (canUndo()) {
-        // historyIndex.current--;
-        // updateQuery();
+        historyIndex.current--;
+        updateQuery();
       }
-      // console.info('undo', history.current, historyIndex.current);
+      console.info("undo", history.current, historyIndex.current);
     };
 
     const redo = () => {
       if (canRedo()) {
-        // historyIndex.current++;
-        // updateQuery();
+        historyIndex.current++;
+        updateQuery();
       }
-      // console.info('redo', history.current, historyIndex.current);
+      console.info("redo", history.current, historyIndex.current);
     };
 
     return {
@@ -132,7 +147,7 @@ function Test({
       redo,
       undo,
     };
-  }, []);
+  }, [modelDef, queryModifiers]);
 
   return (
     <UndoContext.Provider value={undoContext}>
@@ -142,12 +157,12 @@ function Test({
           modelPath="test"
           source={source}
           queryModifiers={queryModifiers}
-          topValues={[]}
+          topValues={topValues}
           queryWriter={queryWriter}
           querySummary={querySummary}
           result={result ?? runnerError ?? builderError}
           runQuery={runQueryAction}
-          // refreshModel={() => setModeDef(structuredClone(modelDef))}
+          refreshModel={refresh}
           isRunning={isRunning}
         />
       </div>
@@ -155,11 +170,35 @@ function Test({
   );
 }
 
+function useRuntimeSetup() {
+  const [setup, setRuntime] = useState<{
+    runtime: malloy.Runtime;
+    modelDef: malloy.ModelDef;
+    refreshModel: () => void;
+  } | null>(null);
+
+  useEffect(() => {
+    async function setup() {
+      console.log("Setting up runtime");
+      const { runtime, model, refreshModel } = await setupRuntime();
+      setRuntime({
+        runtime,
+        modelDef: model._modelDef,
+        refreshModel,
+      });
+    }
+    setup();
+  }, []);
+
+  return setup;
+}
+
 async function setupRuntime() {
   const conn = new DuckDBWASMConnection({ name: "test" });
   const runtime = new SingleConnectionRuntime({ connection: conn });
-  const modelMa = runtime.loadModel(
-    `
+  async function load() {
+    const modelMa = runtime.loadModel(
+      `
     source: test is duckdb.sql("""
     SELECT
         'P' || CAST(ROW_NUMBER() OVER() AS VARCHAR) as product_id,
@@ -173,6 +212,9 @@ async function setupRuntime() {
         CURRENT_DATE - ((RANDOM() * 365)::INT || ' days')::INTERVAL as sale_date
     FROM generate_series(1, 1000)
 """) extend {
+    view: Everything is {
+      select: *
+    }
     view: by_category is {
         group_by: category
         aggregate: product_count is count(product_id)
@@ -184,16 +226,18 @@ async function setupRuntime() {
     }
 }
     `
-  );
-  const model = await modelMa.getModel();
+    );
+    return await modelMa.getModel();
+  }
 
   return {
-    model,
+    model: await load(),
+    refreshModel: load,
     runtime,
   };
 }
 
-async function runQueryExt(
+async function executeMalloyQuery(
   runtime: malloy.Runtime,
   query: string,
   model: malloy.ModelDef
@@ -212,24 +256,46 @@ async function runQueryExt(
   return runnable.run({ rowLimit });
 }
 
-export async function search(
-  model: malloy.ModelDef,
+function useTopValues(
   runtime: malloy.Runtime,
-  source: malloy.StructDef,
-  searchTerm: string,
-  fieldPath?: string
-): Promise<malloy.SearchIndexResult[] | undefined | Error> {
-  const sourceName = source.as || source.name;
-  return runtime
-    ._loadModelFromModelDef(model)
-    .search(sourceName, searchTerm, undefined, fieldPath);
+  model?: malloy.ModelDef,
+  source?: malloy.StructDef,
+  modelPath?: string
+): {
+  refresh: () => void;
+  topValues: malloy.SearchValueMapResult[] | undefined;
+} {
+  const [topValues, setTopValues] = useState<
+    malloy.SearchValueMapResult[] | undefined
+  >();
+  const [refresh, setRefresh] = useState(false);
+
+  useEffect(() => {
+    async function getValues() {
+      setTopValues(await fetchTopValues(runtime, model, source, modelPath));
+    }
+    getValues();
+  }, [model, modelPath, runtime, source, refresh]);
+
+  return {
+    refresh: () => {
+      setRefresh(!refresh);
+    },
+    topValues,
+  };
 }
 
-export async function topValues(
+async function fetchTopValues(
   runtime: malloy.Runtime,
-  model: malloy.ModelDef,
-  source: malloy.StructDef
+  model?: malloy.ModelDef,
+  source?: malloy.StructDef,
+  modelPath?: string
 ): Promise<malloy.SearchValueMapResult[] | undefined> {
+  if (source === undefined || model === undefined) {
+    return undefined;
+  }
+  console.log({ modelPath });
+
   const sourceName = source.as || source.name;
   return runtime._loadModelFromModelDef(model).searchValueMap(sourceName);
 }
