d955d0a509e3cca1e60bdadd09f56a36d430e257
asrar <asrar@qbil.nl>
2025-02-10 22:34:01 +0100

    chore: Adds nix

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs = {
+      url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    };
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-compat }: 
+    let
+    # System types to support.
+    supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+    # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    # Nixpkgs instantiated for supported system types.
+    nixpkgsFor = forAllSystems
+      (system: import nixpkgs {
+        inherit system;
+        config = { allowUnfree = true; };
+      });
+    in {
+
+    devShells = forAllSystems (system:
+      let 
+        pkgs = nixpkgsFor.${system}; 
+      in
+      {
+        default = pkgs.mkShell {
+          buildInputs = [ pkgs.nodejs_22 ];
+          shellHook = ''
+            # To make malloy extension work on nixos
+            export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib}/lib";
+          '';
+        };
+      }
+    );
+  };
+}
