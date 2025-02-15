981e8b848902b6b94f4104cac5236e0ba7836dd4
asrar <asrar@qbil.nl>
2025-02-15 09:44:06 +0100

    chore: Format

--- tests/site.spec.ts
+++ tests/site.spec.ts
@@ -4,7 +4,9 @@ test("Home page loads", async ({ page }) => {
   await page.goto("/");
   await expect(page.getByText("Loading...")).toBeVisible();
   // wait for loading spinner
-  await expect(page.getByText("Loading...")).not.toBeVisible({timeout: 15 * 1000});
+  await expect(page.getByText("Loading...")).not.toBeVisible({
+    timeout: 15 * 1000,
+  });
   await expect(
     page.getByRole("heading", { name: "Malloy model explorer" }),
   ).toBeVisible();
